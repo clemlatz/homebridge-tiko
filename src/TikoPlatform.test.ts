ef68fb627c0afca799eb83d6e3903bceb4bad124
Clément Latzarus <c@ltzr.io>
2024-01-03 15:23:49 +0100

    test: add TikoPlateform tests

---
+++ src/TikoPlatform.test.ts
@@ -0,0 +1,37 @@
+import TikoAPI from './TikoAPI';
+import {API, Logger, PlatformConfig} from 'homebridge';
+import {TikoPlatform} from './TikoPlatform';
+import {TikoApiError} from './TikoApiError';
+
+jest.mock('./TikoAPI');
+
+describe('#discoverDevices', () => {
+  test('authenticates and query all rooms', async () => {
+    // given
+    const logMock = {
+      debug: jest.fn(),
+      error: jest.fn(),
+    } as unknown as Logger;
+    const configMock = {} as PlatformConfig;
+    const homebridgeApi = {
+      hap: {},
+      on: jest.fn(),
+    } as unknown as API;
+
+    const tikoApiMock = {
+      authenticate: jest.fn(),
+      getAllRooms: jest.fn(() => []),
+    } as unknown as TikoAPI;
+    TikoAPI.build = jest.fn(() => tikoApiMock);
+
+    const tikoPlatform = new TikoPlatform(logMock, configMock, homebridgeApi);
+
+    // when
+    await tikoPlatform.discoverDevices();
+
+    // then
+    expect(TikoAPI.build).toHaveBeenCalled();
+    expect(tikoApiMock.authenticate).toHaveBeenCalled();
+    expect(tikoApiMock.getAllRooms).toHaveBeenCalled();
+  });
+});
